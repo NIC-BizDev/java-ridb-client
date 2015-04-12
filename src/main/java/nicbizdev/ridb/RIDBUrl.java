f4f120c9e2146a2e7b406a7950f8ac56c6c0b84a
Jonathan Strange <jonathan@egovtn.org>
2015-04-12 08:22:59 -0500

    Renamed api key param
    Small refactoring to rename API param name.

--- src/main/java/nicbizdev/ridb/RIDBUrl.java
+++ src/main/java/nicbizdev/ridb/RIDBUrl.java
@@ -8,16 +8,16 @@ public class RIDBUrl {
 
 	private static final String BASE_URL = "ridb.recreation.gov/api/v1/";
 	private static final String SCHEME = "https";
-	private static final String API_PARAM = "apikey";
+	private static final String API_PARAM_NAME = "apikey";
 
 	public static String createRidbUrl(String path, String apiKey) {
 		return UriComponentsBuilder.newInstance().scheme(SCHEME).host(BASE_URL)
-				.path(path).queryParam(API_PARAM, apiKey).build().toUriString();
+				.path(path).queryParam(API_PARAM_NAME, apiKey).build().toUriString();
 	}
 
 	public static String createRidbUrl(String path, String apiKey, Map<String, Object> params) {
 		UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
-				.scheme(SCHEME).host(BASE_URL).path(path).queryParam(API_PARAM, apiKey);
+				.scheme(SCHEME).host(BASE_URL).path(path).queryParam(API_PARAM_NAME, apiKey);
 		for (Map.Entry<String, Object> param : params.entrySet()) {
 			builder.queryParam(param.getKey(), param.getValue());
 		}
